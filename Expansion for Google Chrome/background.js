b29af8ee7b561d38d981fee8eb0950e77aa27d0d
YuKars1996 <lyly1001@yandex.ru>
2019-05-12 11:24:03 +0300

    Create Expansion for Google Chrome

---
+++ Expansion for Google Chrome/background.js
@@ -0,0 +1,26 @@
+﻿let vopros = confirm('Будем открывать ссылки?'); #всплывающее окно, подтверждение действия
+
+if (vopros == true){
+
+let cnt = 0;
+    let link = document.getElementsByTagName('a'); #все ссылки на странице
+    for(let i = 0; i < link.length; i++){ #пока мы видим все ссылки
+#задаем условия для ссылок
+        if(link[i].href.indexOf("#адрес, по которому ищем") > -1 && link[i].className == 'pull-right') {
+            link[i].innerHTML = '<p>оки</p>'; #если ссылка открыта, вставляем тег р
+            window.open(link[i].href); #открываем в новом окне
+            cnt++;   
+            
+        }
+    }
+if(cnt) {
+    alert('Ура, мы все открыли!');
+} else {
+    console.log('Что-то пошло не так, или нет ссылок');
+    alert('А ссылок-то нет...')
+}
+}   
+
+          
+                                
+
